f36690ad56df4374e4005132c931e59454dfc801
abar ahmed <abrarahmed2185@gmail.com>
2022-08-04 14:27:31 +0500

    Setup User Route

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ const path = require("path");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
+const userRoutes  = require("./routes/user");
 
 const port = process.env.PORT || 8080;
 
 // connected to Database ...
 const connectDB = require("./database/db");
 
-// Connecting to database
+
+// Setup User Routes
+
+app.use('/api/users',userRoutes)
+
+// User Routes
 
 connectDB()
   .then(() => {
     console.log("Database Connected Successfully!");
   })
   .catch((err) =>
+
     console.log("Server Failed . No connection to Database  ", err.message)
   );
 
---
+++ routes/user.js
@@ -0,0 +1,19 @@
+// *Importing Router from express
+const router = require("express").Router();
+
+// *Importing User Model
+const User = require("../models/User");
+
+
+
+//* Creating a User
+
+router.post('/register', (req, res) => {
+
+    const user = new User(req.body);
+
+})
+
+
+
+module.exports = router
